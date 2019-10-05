16a8bc8c599ac3496260176c03ebda29c0c93293
LUIS <Ingenieroluistafur@gmail.com>
2019-10-05 12:18:32 -0500

    new commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBXrfIpaUSdabA72bO4b_atuk5whdU2HW0",
+    authDomain: "academiamoviles-66fe5.firebaseapp.com",
+    databaseURL: "https://academiamoviles-66fe5.firebaseio.com",
+    projectId: "academiamoviles-66fe5",
+    storageBucket: "academiamoviles-66fe5.appspot.com",
+    messagingSenderId: "541599594356",
+    appId: "1:541599594356:web:256b105c22c7b9bf"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyBXrfIpaUSdabA72bO4b_atuk5whdU2HW0",
     authDomain: "academiamoviles-66fe5.firebaseapp.com",
